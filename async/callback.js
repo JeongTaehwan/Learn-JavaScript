00b19c15298fd77807a890cceba9ef173aa32702
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-03-03 14:27:25 +0900

    add commit
    add commit

---
+++ async/callback.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//동기 비동기
+//hoisting: var,함수 선언 등이 자동적으로 제일 위로 올라가는 것
+
+console.log('1');
+setTimeout(() => console.log('2'), 500); 
+console.log('3');
+
+// 콜백 마지막 정리
+
+//즉각
+function printImmediately(print) {
+    print();
+}
+
+printImmediately(() => console.log('hello'));
+
+function prinWithDelay(print, timeout) {
+
+}
