9f79edb188f6295d84ef7247ff71dd519e8525a6
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-03-03 17:17:22 +0900

    [ADD async]
    [ADD async]

--- async/callback.js
+++ async/callback.js
@@ -16,6 +16,8 @@ function printImmediately(print) {
 
 printImmediately(() => console.log('hello'));
 
-function prinWithDelay(print, timeout) {
-
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
 }
+
+printWithDelay(() => console.log('async callback'), 2000);
