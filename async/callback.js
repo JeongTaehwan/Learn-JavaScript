d38127852c7b807c57a6e20e971538a8654b5bab
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-03-04 10:21:30 +0900

    [ADD CallBack]
    [ADD CallBack]

--- async/callback.js
+++ async/callback.js
@@ -3,11 +3,15 @@
 //동기 비동기
 //hoisting: var,함수 선언 등이 자동적으로 제일 위로 올라가는 것
 
+//동기
+console.log('1');
+console.log('2');
+console.log('3');
+
+//비동기
 console.log('1');
 setTimeout(() => console.log('2'), 1000); 
 console.log('3');
-
-// 콜백 마지막 정리
 
 //즉각
 function printImmediately(print) {
@@ -16,8 +20,57 @@ function printImmediately(print) {
 
 printImmediately(() => console.log('hello'));
 
-function prinWithDelay(print, timeout) {
+//비동기 CallBack
+function printWithDelay(print, timeout) {
     setTimeout(print, timeout);
 }
 
 printWithDelay(() => console.log('async callback'), 2000);
+
+//콜백지옥 예제
+
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(() => {
+            if (
+                (id === 'Puwan' && password === 'Taehwan') ||
+                (id === 'coder' && password === 'good')
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not Found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if(user === 'Puwan') {
+                onSuccess({ name: 'Puwan', role: 'admin'});
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('이름을 입력하세요');
+const password = prompt('비밀번호를 입력하세요');
+
+userStorage.loginUser(
+    id, 
+    password, 
+    user => {
+        userStorage.getRoles(
+            user, 
+            userWithRole => {
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    },
+    error => {console.log(error)}
+);
